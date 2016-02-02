ba59d52d25ebc9bb9a8160ed3df96e2f3ecf3613
ebrunetta <esteban.brunetta@gmail.com>
2016-02-02 07:59:58 -0300

    agregar vista de asignacion de turnos

---
+++ app/scripts/controllers/logincontroler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adamantiumMobileApp.controller:LogincontrolerCtrl
+ * @description
+ * # LogincontrolerCtrl
+ * Controller of the adamantiumMobileApp
+ */
+angular.module('adamantiumMobileApp')
+  .controller('LogincontrolerCtrl', function ($scope) {
+  	$scope.loguearse = function()
+  	{
+  		
+  	};
+  });
---
+++ app/scripts/controllers/solicitarturnos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adamantiumMobileApp.controller:SolicitarturnosCtrl
+ * @description
+ * # SolicitarturnosCtrl
+ * Controller of the adamantiumMobileApp
+ */
+angular.module('adamantiumMobileApp')
+  .controller('SolicitarturnosCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
